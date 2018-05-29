6c2fcc39235b8a905e9c6f6df2e7ab8f8f0ac0fc
maks <maxim_86nva@mail.ru>
2018-05-29 21:38:14 +0300

    update

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,3 @@
 #!/usr/bin/env node
-import userName from '../'
+import {greeting} from '..'
+greeting()
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import readlineSync from 'readline-sync'
-const userName = () => readlineSync.question('May I have your name?: ')
-console.log('Welcome to the Brain Games!')
-console.log(`Hello, ${userName()}!`)
+
+export const greeting = () => {
+  const userName = () => readlineSync.question('May I have your name?: ')
+  console.log('Welcome to the Brain Games!')
+  console.log(`Hello, ${userName()}!`)
+}
