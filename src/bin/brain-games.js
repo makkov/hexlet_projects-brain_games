6772e53f8b86991cbf23a9da9d70874e3241a07a
maks <maxim_86nva@mail.ru>
2018-05-29 12:45:37 +0300

    fix

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,4 +1,2 @@
 #!/usr/bin/env node
-import userName from '../index'
-console.log('Welcome to the Brain Games!');;
-console.log(`Hello, ${userName()}!`)
+import userName from '../'
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import readlineSync from 'readline-sync'
 const userName = () => readlineSync.question('May I have your name?: ')
-export default userName
+console.log('Welcome to the Brain Games!')
+console.log(`Hello, ${userName()}!`)
